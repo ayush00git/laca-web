07951cd78bc6d8d54c4e194a6982fc9687923ca7
ayush00git <ayush.2007.iit@gmail.com>
2026-04-06 00:27:36 +0530

    pkg: db connection helper added

---
+++ db/connect.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectToMongo (uri string) (*mongo.Database, error) {
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("MongoDB connected!")
+	return client.Database("laca-db"), nil
+}
